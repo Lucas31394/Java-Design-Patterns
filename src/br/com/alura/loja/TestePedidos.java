b52f8c079a682c193cbbf65d206f6eb846f07f4d
Lucas Pereira <lucaspfernandes1994@hotmail.com>
2021-09-22 13:28:13 -0300

    Aplicando o pattern Observer

--- src/br/com/alura/loja/TestePedidos.java
+++ src/br/com/alura/loja/TestePedidos.java
@@ -1,19 +1,22 @@
 package br.com.alura.loja;
 
 import java.math.BigDecimal;
+import java.util.Arrays;
 
 import br.com.alura.loja.pedido.GeraPedido;
 import br.com.alura.loja.pedido.GeraPedidoHandler;
+import br.com.alura.loja.pedido.acao.EnviarEmailPedido;
+import br.com.alura.loja.pedido.acao.SalvarPedidoNoBancoDeDados;
 
 public class TestePedidos {
 	
 	public static void main(String[] args) {
-		String cliente = args[0];
-		BigDecimal valorOrcamento = new BigDecimal(args[1]);
-		int quantidadeDeItens = Integer.parseInt(args[2]);
+		String cliente = "Jhon";
+		BigDecimal valorOrcamento = new BigDecimal("300");
+		int quantidadeDeItens = Integer.parseInt("2");
 		
 		GeraPedido gerador = new GeraPedido(cliente, valorOrcamento, quantidadeDeItens);
-		GeraPedidoHandler handler = new GeraPedidoHandler(/*dependencias*/);
+		GeraPedidoHandler handler = new GeraPedidoHandler(Arrays.asList(new SalvarPedidoNoBancoDeDados(), new EnviarEmailPedido()));
 		
 		handler.execute(gerador);
 	}
--- src/br/com/alura/loja/pedido/GeraPedidoHandler.java
+++ src/br/com/alura/loja/pedido/GeraPedidoHandler.java
@@ -1,24 +1,27 @@
 package br.com.alura.loja.pedido;
 
 import java.time.LocalDateTime;
+import java.util.List;
 
 import br.com.alura.loja.orcamento.Orcamento;
-import br.com.alura.loja.pedido.acao.EnviarEmailPedido;
-import br.com.alura.loja.pedido.acao.SalvarPedidoNoBancoDeDados;
+import br.com.alura.loja.pedido.acao.AcaoAposGerarPedido;
 
 public class GeraPedidoHandler {
 	
+	private List<AcaoAposGerarPedido> acoes;
+	
+	public GeraPedidoHandler(List<AcaoAposGerarPedido> acoes) {
+		this.acoes = acoes;
+	}
+
+
+
 	public void execute(GeraPedido dados) {
 		Orcamento orcamento = new Orcamento(dados.getValorOrcamento(), dados.getQuantidadeItens());
-		String cliente = dados.getCliente();
 		
-		Pedido pedido = new Pedido(cliente, LocalDateTime.now(), orcamento);
+		Pedido pedido = new Pedido(dados.getCliente(), LocalDateTime.now(), orcamento);
 		
-		EnviarEmailPedido email = new EnviarEmailPedido();
-		SalvarPedidoNoBancoDeDados salvarNoBanco = new SalvarPedidoNoBancoDeDados();
-		
-		email.executar(pedido);
-		salvarNoBanco.executar(pedido);
+		acoes.forEach(a -> a.executarAcao(pedido));
 	}
 
 }
